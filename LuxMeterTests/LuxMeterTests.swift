44cd6a5898b6c1e2a57475467dd6d0d9b84f5e2c
dad <support@dadlink.co.uk>
2024-12-28 06:08:55 +0000

    Initial Commit

---
+++ LuxMeterTests/LuxMeterTests.swift
@@ -0,0 +1,16 @@
+//
+//  LuxMeterTests.swift
+//  LuxMeterTests
+//
+//  Created by mathew on 28/12/2024.
+//
+
+import Testing
+
+struct LuxMeterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
